caccb0324bb2ee1b91a594b370c601ff52326a9f
cjdufault <dufaultconnor@gmail.com>
2020-02-29 10:41:36 -0600

    added handling for invalid SNA and plant names, as well as SNA's that don't have plants documented

--- app.py
+++ app.py
@@ -27,32 +27,38 @@ def sna_search():
 @app.route("/sna/<sna_name>")
 def sna_page(sna_name):
     try:
-        sna_id = search_results[sna_name]  # get id from existing search results
+        try:
+            sna_id = search_results[sna_name]  # get id from existing search results
+        except KeyError:
+            sna_id = dnr.search(sna_name)[sna_name]  # if that fails, do a search again to get id
+
+        sna_object = dnr.sna_details_request(sna_id)
+
+        return render_template("sna_page.html",
+                               name=sna_object.name,
+                               tags=sna_object.tags,
+                               county=sna_object.county,
+                               desc=sna_object.desc,
+                               notes=sna_object.notes,
+                               directions=sna_object.directions,
+                               wildflowers=sna_object.wildflowers,
+                               grasses=sna_object.grasses,
+                               trees_shrubs=sna_object.trees_shrubs)
     except KeyError:
-        sna_id = dnr.search(sna_name)[sna_name]  # if that fails, do a search again to get id
-
-    sna_object = dnr.sna_details_request(sna_id)
-
-    return render_template("sna_page.html",
-                           name=sna_object.name,
-                           tags=sna_object.tags,
-                           county=sna_object.county,
-                           desc=sna_object.desc,
-                           notes=sna_object.notes,
-                           directions=sna_object.directions,
-                           wildflowers=sna_object.wildflowers,
-                           grasses=sna_object.grasses,
-                           trees_shrubs=sna_object.trees_shrubs)
+        return f"<h1>No result found for \"{sna_name}\"</h1>\n" \
+               f"<a href=\"/\">Return home</a>"
 
 
 @app.route("/plant/<sci_name>")
 def plant_page(sci_name):
-    print(sci_name)
-    plant = trefle.search(sci_name)
-
-    return render_template("plant_page.html",
-                           cname=plant.common_name,
-                           sname=plant.sci_name)
+    try:
+        plant = trefle.search(sci_name)
+        return render_template("plant_page.html",
+                               cname=plant.common_name,
+                               sname=plant.sci_name)
+    except AttributeError:
+        return f"<h1>No result found for \"{sci_name}\"</h1>\n" \
+               f"<a href=\"/\">Return home</a>"
 
 
 if __name__ == "__main__":

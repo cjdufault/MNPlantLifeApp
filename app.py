55479388d11046dc1e686675ecd42b6e497c0138
cjdufault <dufaultconnor@gmail.com>
2020-03-10 16:03:40 -0500

    added an "about" page that describes what SNAs are and what the website does

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template
 import dnr_apis as dnr
 import trefle_api as trefle
 import os
